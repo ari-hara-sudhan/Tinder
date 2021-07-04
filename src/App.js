68c9978abfec75db86dabd4edc63cb8e695e360a
=ari <=ahshari2003@gmail.com>
2021-07-04 18:45:16 +0530

    add some files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./App.css"
+
+function App() {
+    return (
+        <div className="app">
+            <h2>hello world</h2>
+            
+        </div>
+    )
+}
+
+export default App
